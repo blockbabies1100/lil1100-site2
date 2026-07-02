27e6ceee27ebc740fb1a29a6d31f70fc0f969a22
blockbabies1100 <blockbabies1100@gmail.com>
2026-07-02 05:15:00 -0400

    Create Home component with layout structure

---
+++ components/App/page.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import LatestRelease from "@/components/LatestRelease";
+import Playlist from "@/components/Playlist";
+import Gallery from "@/components/Gallery";
+import About from "@/components/About";
+import Article from "@/components/Article";
+import VideoGallery from "@/components/VideoGallery";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <LatestRelease />
+      <Playlist />
+      <VideoGallery />
+      <About />
+      <Gallery />
+      <Article />
+      <Footer />
+    </>
+  );
+}
