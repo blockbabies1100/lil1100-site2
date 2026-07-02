88bb6628258de609e4546ec87490fb860b62d204
blockbabies1100 <blockbabies1100@gmail.com>
2026-07-02 14:58:17 -0400

    Add PageTransition component for animated transitions

---
+++ components/Pagetransition.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+export default function PageTransition({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.main
+        key={pathname}
+        initial={{ opacity: 0, y: 25 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -25 }}
+        transition={{
+          duration: 0.45,
+          ease: "easeInOut",
+        }}
+      >
+        {children}
+      </motion.main>
+    </AnimatePresence>
+  );
+}
