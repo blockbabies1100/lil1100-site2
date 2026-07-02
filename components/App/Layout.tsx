06e8a6f97da738954b15f11ab6cca18a20e018b8
blockbabies1100 <blockbabies1100@gmail.com>
2026-07-02 14:50:44 -0400

    Add metadata for Lil 1100 official website
    Added metadata for the official website including title, description, and keywords.

---
+++ components/App/Layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Lil 1100 | Official Website",
+  description:
+    "Official website of Lil 1100. Music, videos, gallery, tour dates, booking and merchandise.",
+
+  keywords: [
+    "Lil 1100",
+    "Rap",
+    "Hip Hop",
+    "Music",
+    "DawgShit",
+    "MADE MEN",
+    "Life We Live
